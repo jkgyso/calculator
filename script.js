bd37fed68af17faa530cd84f91c4d46c9f8957fc
jkgyso <jkgyso@gmail.com>
2025-06-07 11:51:41 +0800

    add project layout

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('Hello World!');
+const inputValue = document.querySelector('#input-value');
+
+const valuesContainer = document.querySelector('#values-container')
+
+
+let num1 = 3; 
+let num2 = 5;
+let operation = '+'
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+
+function operate(operator, num1, num2) {
+    if(operator == '+') {
+        return add(num1, num2);
+    } else if(operator == '-') {
+        return subtract(num1, num2);
+    } else if(operator == '*') {
+        return multiply(num1, num2);
+    } else if(operator == '/') {
+        return divide(num1, num2)
+    }
+}
+
+
+function enterValues() {
+    let calculatorValues = document.querySelectorAll('.value');
+    
+
+    
+}
+
+valuesContainer.addEventListener('click', enterValues);
